6c6e869b8a9c46ec4c6b13131536c69167e9b98e
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-08-13 00:34:22 -0300

    Aplica passagem de argumento à busca de professores

---
+++ lista_profs03.py
@@ -0,0 +1,17 @@
+import sys
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = 'http://www.fatecjd.edu.br/site/a-fatec-jd/corpo-docente'
+html = urlopen(url)
+
+bsObj = BeautifulSoup(html.read(), 'html.parser')
+
+body = bsObj.tbody
+rows = body.findAll('tr')
+
+for row in rows:
+    name = row.find('td').getText()
+
+    if sys.argv[1].upper() in name:
+        print(row.getText())
