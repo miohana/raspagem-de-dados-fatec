e24ca873c72d60da22b7ebdac8fc5420c99ea6cd
Lidiane Taquehara <lidi.mayra@gmail.com>
2017-08-18 21:07:59 -0300

    Lista as notícias mais recentes de todas as tags

---
+++ cultura_jundiai02.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = 'http://cultura.jundiai.sp.gov.br/'
+html = urlopen(url)
+bsObj = BeautifulSoup(html.read(), 'html.parser')
+
+tag = bsObj.find('div', { 'class': 'tagcloud' })
+links = tag.findAll('a')
+
+for link in links:
+    url = link['href'] 
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html.read(), 'html.parser')
+
+    lista_titulos = bsObj.findAll('a', { 'class': 'titulo-lista' })
+
+    for titulo in lista_titulos:
+        text = titulo.text
+        print(text)
